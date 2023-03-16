b5dcf416116d05f743ee2b6909c10b129506d6d4
Priyanshu <priyanshuanand60@gmail.com>
2023-03-16 00:08:34 -0700

    first commit

---
+++ app/src/main/java/com/example/application_generator/Home.java
@@ -0,0 +1,14 @@
+package com.example.application_generator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
